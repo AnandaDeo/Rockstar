8e59b5a34a39dcfd670623f3073cdea5ada63689
DarCode <deomastar76@gmail.com>
2022-11-02 16:19:55 +0700

    menambahkan resep

---
+++ Resep.java
@@ -0,0 +1,10 @@
+package Rockstar;
+
+public class Resep {
+    public static void main(String[] args) {
+        System.out.println("Resep Rockstar");
+        System.out.println("------------------------------------------------");
+        System.out.println("1. Codingan");
+        System.out.println("2. Design");
+    }
+}
